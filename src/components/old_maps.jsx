c11edd54b2d30e14213c6665fcb9b53b615a71f7
RobertLudwick <rludwick@drew.edu>
2023-03-02 17:45:25 -0500

    map updates and zoom limits

--- src/components/old_maps.jsx
+++ src/components/old_maps.jsx
@@ -9,8 +9,13 @@ class Old_maps extends Component{
     render(){
         return (
             <div>
-                <MapContainer className='map' center={[1, 1]} zoom={0} scrollWheelZoom={false} >
-                    <TileLayer url = 'https://raw.githubusercontent.com/RobertLudwick/DndMapApp/main/public/Maps/TileGroup0/{z}-{x}-{y}.png' noWrap={true}/>
+                <MapContainer className='map' center={[1, 1]} zoom={0} minZoom={0} maxZoom={5} scrollWheelZoom={false} >
+                    <TileLayer url = 'https://raw.githubusercontent.com/RobertLudwick/DndMapApp/main/public/Maps/TileGroup0/{z}-{x}-{y}.jpg' noWrap={true}/>
+                    <TileLayer url = 'https://raw.githubusercontent.com/RobertLudwick/DndMapApp/main/public/Maps/TileGroup1/{z}-{x}-{y}.jpg' noWrap={true}/>
+                    <TileLayer url = 'https://raw.githubusercontent.com/RobertLudwick/DndMapApp/main/public/Maps/TileGroup2/{z}-{x}-{y}.jpg' noWrap={true}/>
+                    <TileLayer url = 'https://raw.githubusercontent.com/RobertLudwick/DndMapApp/main/public/Maps/TileGroup3/{z}-{x}-{y}.jpg' noWrap={true}/>
+                    <TileLayer url = 'https://raw.githubusercontent.com/RobertLudwick/DndMapApp/main/public/Maps/TileGroup4/{z}-{x}-{y}.jpg' noWrap={true}/>
+                    <TileLayer url = 'https://raw.githubusercontent.com/RobertLudwick/DndMapApp/main/public/Maps/TileGroup5/{z}-{x}-{y}.jpg' noWrap={true}/>
                 </MapContainer> 
             </div>
 	  
