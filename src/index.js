1d71f4602a3d8c15e82aa17d141c3d6bcea65b98
RobertLudwick <rludwick@drew.edu>
2023-03-02 16:06:35 -0500

    reverted to old method

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import maps from './components/maps'
-import old_maps from './components/old_maps';
 
 import './index.css';
 import 'leaflet/dist/leaflet.css'
