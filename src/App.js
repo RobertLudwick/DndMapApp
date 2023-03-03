669a47d20de293ca32be36977a5d5ce234571226
RobertLudwick <rludwick@drew.edu>
2023-03-03 17:34:34 -0500

    starting on marker sorted out markdown files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import './App.css';
-import Maps from './components/maps';
 import Old_maps from './components/old_maps';
 
 import 'leaflet/dist/leaflet.css'
--- src/components/old_maps.jsx
+++ src/components/old_maps.jsx
@@ -1,21 +1,33 @@
 import React, { Component } from "react";
-import {MapContainer, TileLayer} from 'react-leaflet'
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 
 import 'leaflet/dist/leaflet.css'
 import '../Style/map.css';
 
+const position = [51.505, -0.09]
+
 class Old_maps extends Component{
+    
     state = {}
     render(){
         return (
             <div>
-                <MapContainer className='map' center={[1, 1]} zoom={0} minZoom={0} maxZoom={5} scrollWheelZoom={false} >
+                {/* <Leaflet.Map ref='leaflet-map' bounds={this.getBounds()} bounds={this.ShowBounds()} boundsOptions={{padding: [50, 50]}} > */}
+
+                <MapContainer ref='leaflet-map' className='map' center={[1, 1]} zoom={0} minZoom={0} maxZoom={5} scrollWheelZoom={false} >
                     <TileLayer url = 'https://raw.githubusercontent.com/RobertLudwick/DndMapApp/main/public/Maps/TileGroup0/{z}-{x}-{y}.jpg' noWrap={true}/>
                     <TileLayer url = 'https://raw.githubusercontent.com/RobertLudwick/DndMapApp/main/public/Maps/TileGroup1/{z}-{x}-{y}.jpg' noWrap={true}/>
                     <TileLayer url = 'https://raw.githubusercontent.com/RobertLudwick/DndMapApp/main/public/Maps/TileGroup2/{z}-{x}-{y}.jpg' noWrap={true}/>
                     <TileLayer url = 'https://raw.githubusercontent.com/RobertLudwick/DndMapApp/main/public/Maps/TileGroup3/{z}-{x}-{y}.jpg' noWrap={true}/>
                     <TileLayer url = 'https://raw.githubusercontent.com/RobertLudwick/DndMapApp/main/public/Maps/TileGroup4/{z}-{x}-{y}.jpg' noWrap={true}/>
                     <TileLayer url = 'https://raw.githubusercontent.com/RobertLudwick/DndMapApp/main/public/Maps/TileGroup5/{z}-{x}-{y}.jpg' noWrap={true}/>
+
+
+                    <Marker position={position}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
                 </MapContainer> 
             </div>
 	  
