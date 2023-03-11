19112be1527d3eaa593f574712826412d936c3af
RobertLudwick <rludwick@drew.edu>
2023-03-10 20:32:35 -0500

    test tiler 5

--- src/components/maps.jsx
+++ src/components/maps.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
+
 import 'leaflet/dist/leaflet.css';
 import '../Style/map.css';
 
 export default class Maps extends Component{
     render(){
         return (
-          <MapContainer ref='leaflet-map' className='map' center={[1, 1]} zoom={0} minZoom={0} maxZoom={5} scrollWheelZoom={false} >
+          <MapContainer ref='leaflet-map' className='map' center={[40.600699, -74.252789]} zoom={15} minZoom={11} maxZoom={16} scrollWheelZoom={false} >
             <TileLayer url = 'https://raw.githubusercontent.com/RobertLudwick/DndMapApp/main/public/TestTile/MapTilerTest-Tile5/{z}/{x}/{y}.png' noWrap={true}/>
           </MapContainer>
         )
