a902a521899ed1653a5291e1e9964118af029b59
RobertLudwick <rludwick@drew.edu>
2023-03-15 11:45:19 -0400

    base marker functionality

--- src/components/maps.jsx
+++ src/components/maps.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
+import Markergroup from "./markergroup";
 
 import 'leaflet/dist/leaflet.css';
 import '../Style/map.css';
 
+const position = [40.600699, -74.252789]
+
 export default class Maps extends Component{
     render(){
         return (
+          <div>
           <MapContainer ref='leaflet-map' className='map' center={[40.600699, -74.252789]} zoom={15} minZoom={11} maxZoom={16} scrollWheelZoom={false} >
             <TileLayer url = 'https://raw.githubusercontent.com/RobertLudwick/DndMapApp/main/public/TestTile/MapTilerTest-Tile5/{z}/{x}/{y}.png' noWrap={true}/>
+            <Markergroup/>
           </MapContainer>
+
+          
+          </div>
         )
     }
 
